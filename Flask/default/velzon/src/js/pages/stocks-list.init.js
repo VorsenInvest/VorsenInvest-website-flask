f62e578aa00549e123ffd2c5fbd981182913c337
VorsenInvest <gustavo.vorsen@gmail.com>
2024-01-24 23:17:33 -0300

    Menu with subsector is working

--- Flask/default/velzon/src/js/pages/stocks-list.init.js
+++ Flask/default/velzon/src/js/pages/stocks-list.init.js
@@ -1,77 +1,82 @@
 $(document).ready(function() {
-    $('.cq-dropdown').dropdownCheckboxes();
-});
-/*
-Template Name: Velzon - Admin & Dashboard Template
-Author: Themesbrand
-Website: https://Themesbrand.com/
-Contact: Themesbrand@gmail.com
-File: stocks-list init js
-*/
+    $('#selectAll').change(function() {
+        $('.subsector-option').prop('checked', this.checked);
+    });
 
-// List Js
-var perPage = 10;
-
-//Table
-var options = {
-    valueNames: [
-        "ticker_name",
-        "economic_sector_name",
-        "subsector_name",
-        "segment_name",
-    ],
-    page: perPage,
-    pagination: true,
-    plugins: [
-        ListPagination({
-            left: 2,
-            right: 2
-        })
-    ]
-};
-
-
-
-function filterData(){
-    var iseconomicsector = document.getElementById("idEconomicSector").value;
-    var issubsector = document.getElementById("idSubSector").value;
-    var issegment = document.getElementById("idSegment").value;
-
-    contactList.filter(function (data) {
-        matchData = new DOMParser().parseFromString(data.values().status, "text/html");
-        var status = matchData.body.firstElementChild.innerHTML;
-        var economicsectorFilter = false;
-        var subsectorFilter = false;
-        var segmentFilter = false;
-
-
-        if (status == "all" || iseconomicsector == "all") {
-            economicsectorFilter = true;
+    $('.subsector-option').change(function() {
+        if (!this.checked) {
+            $('#selectAll').prop('checked', false);
         } else {
-            economicsectorFilter = status == iseconomicsector;
-        }
-
-        if (status == "all" || issubsector == "all") {
-            subsectorFilter = true;
-        } else {
-            subsectorFilter = status == issubsector;
-        }
-
-        if (status == "all" || issegment == "all") {
-            segmentFilter = true;
-        } else {
-            segmentFilter = status == issegment;
-        }
-
-
-        if(economicsectorFilter && subsectorFilter && segmentFilter){
-            return economicsectorFilter && subsectorFilter && segmentFilter
-        }  else if (economicsectorFilter && subsectorFilter && pickerVal == "") {
-            return economicsectorFilter && subsectorFilter;
-        } else if (subsectorFilter && segmentFilter && pickerVal == "") {
-            return subsectorFilter && segmentFilter;
+            var allChecked = $('.subsector-option').length === $('.subsector-option:checked').length;
+            $('#selectAll').prop('checked', allChecked);
         }
     });
 
-    contactList.update();
-}
+
+    // List.js related code
+    var options = {
+        valueNames: [
+            "ticker_name",
+            "economic_sector_name",
+            "subsector_name",
+            "segment_name",
+        ],
+        page: perPage,
+        pagination: true,
+        plugins: [
+            ListPagination({
+                left: 2,
+                right: 2
+            })
+        ]
+    };
+
+    function filterData(){
+        var iseconomicsector = document.getElementById("idEconomicSector").value;
+        var issubsector = document.getElementById("idSubSector").value;
+        var issegment = document.getElementById("idSegment").value;
+    
+        contactList.filter(function (data) {
+            matchData = new DOMParser().parseFromString(data.values().status, "text/html");
+            var status = matchData.body.firstElementChild.innerHTML;
+            var economicsectorFilter = false;
+            var subsectorFilter = false;
+            var segmentFilter = false;
+    
+    
+            if (status == "all" || iseconomicsector == "all") {
+                economicsectorFilter = true;
+            } else {
+                economicsectorFilter = status == iseconomicsector;
+            }
+    
+            if (status == "all" || issubsector == "all") {
+                subsectorFilter = true;
+            } else {
+                subsectorFilter = status == issubsector;
+            }
+    
+            if (status == "all" || issegment == "all") {
+                segmentFilter = true;
+            } else {
+                segmentFilter = status == issegment;
+            }
+    
+    
+            if(economicsectorFilter && subsectorFilter && segmentFilter){
+                return economicsectorFilter && subsectorFilter && segmentFilter
+            }  else if (economicsectorFilter && subsectorFilter && pickerVal == "") {
+                return economicsectorFilter && subsectorFilter;
+            } else if (subsectorFilter && segmentFilter && pickerVal == "") {
+                return subsectorFilter && segmentFilter;
+            }
+        });
+    
+        contactList.update();
+    }
+});
+
+
+
+
+
